a7b855c25e1022df37a0687715f536fab69998a6
Lucas Medeiros <lucas_medeiros09@hotmail.com>
2016-03-22 20:39:16 -0300

    Aula FX e Scene Builder

---
+++ src/br/univel/TelaPrincipalController.java
@@ -0,0 +1,25 @@
+package br.univel;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
+public class TelaPrincipalController {
+
+    @FXML
+    private Label label;
+
+    @FXML
+    private TextField campo;
+
+    @FXML
+    private Button botao;
+
+    @FXML
+    void executar(ActionEvent event) {
+    	System.out.println(campo.getText());
+    }
+
+}
